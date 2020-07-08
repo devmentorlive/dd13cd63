67fbd30d36586bb156382d949fe53a51127e4da6
Mark Tellez <mark.tellez@icloud.com>
2020-07-08 11:45:23 -0500

    base starter

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="page-container">
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
