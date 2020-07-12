7fba4d3ac720310e479f5503d3203ab77650efd8
Mark Tellez <mark.tellez@icloud.com>
2020-07-11 19:05:17 -0500

    better grass tiles

--- src/components/zone/index.jsx
+++ src/components/zone/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import zones from "../../data/zones";
 
 export default function Zone({ children, tileset }) {
-  const zoneData = zones[tileset];
   const terrain = require(`../../data/zones/${tileset}/terrain`).default;
   const tiles = require(`../../data/zones/${tileset}`).default.tiles;
 
@@ -13,10 +11,8 @@ export default function Zone({ children, tileset }) {
         <div className="row">
           {row.map((tile) => {
             const image = `/sprites/zones/${tileset}.png`;
-            const x = tiles[tile.toString()][1];
-            const y = tiles[tile.toString()][2];
-            const w = tiles[tile.toString()][3];
-            const h = tiles[tile.toString()][4];
+            const tileData = tiles[tile.toString()];
+            const [name, x, y, w, h] = tileData;
 
             return (
               <div
