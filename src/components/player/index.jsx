e0cd7a44dd7e843bcffff49b80ec16fbd31c9bea
Mark Tellez <mark.tellez@icloud.com>
2020-07-13 03:57:50 -0500

    walks

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -4,15 +4,14 @@ import useKeyPress from "../../hooks/use-key-press";
 import useWalk from "../../hooks/use-walk";
 
 export default function Player({ skin }) {
-  const { dir, step, walk } = useWalk(3);
+  const { dir, step, walk, position } = useWalk(3);
   const data = {
     h: 32,
     w: 32,
   };
 
   useKeyPress((e) => {
-    const dir = e.key.replace("Arrow", "").toLowerCase();
-    walk(dir);
+    walk(e.key.replace("Arrow", "").toLowerCase());
 
     e.preventDefault();
   });
@@ -23,6 +22,10 @@ export default function Player({ skin }) {
       data={data}
       step={step}
       dir={dir}
+      position={position}
+      style={{
+        position: "absolute",
+      }}
     />
   );
 }
